4a147d5c585099878b4101053d41ab8b224cac39
João Marcello <marcello.jao1611@gmail.com>
2025-05-20 18:58:22 -0300

    consulta sql, projection, search game by list

--- src/main/java/com/jota/dslist/services/GameService.java
+++ src/main/java/com/jota/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.jota.dslist.dto.GameDTO;
 import com.jota.dslist.dto.GameMinDTO;
 import com.jota.dslist.entities.Game;
+import com.jota.dslist.projections.GameMinProjection;
 import com.jota.dslist.repositories.GameRepository;
 
 @Service
@@ -26,6 +27,12 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 	}
 }
